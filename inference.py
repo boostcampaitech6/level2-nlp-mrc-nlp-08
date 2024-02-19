fa84afffb57733a3e41390a244ec2ff5c83b981b
jsdysw <jsdysw@gmail.com>
2024-02-19 08:59:53 +0000

    refactor: modify too much..

--- inference.py
+++ inference.py
@@ -4,23 +4,18 @@ Open-Domain Question Answering 을 수행하는 inference 코드 입니다.
 대부분의 로직은 train.py 와 비슷하나 retrieval, predict 부분이 추가되어 있습니다.
 """
 
-
 import logging
 import sys
-from typing import Callable, Dict, List, NoReturn, Tuple
+from typing import Dict, NoReturn
+from functools import partial
 
 import numpy as np
 from arguments import DataTrainingArguments, ModelArguments
 from datasets import (
-    Dataset,
     DatasetDict,
-    Features,
-    Sequence,
-    Value,
     load_from_disk,
     load_metric,
 )
-from retrieval import SparseRetrieval
 from trainer_qa import QuestionAnsweringTrainer
 from transformers import (
     AutoConfig,
@@ -32,7 +27,10 @@ from transformers import (
     TrainingArguments,
     set_seed,
 )
-from utils_qa import check_no_error, postprocess_qa_predictions
+from utils_qa import check_no_error
+from retrieval.retrieval_utils import run_sparse_retrieval
+from mrc.preprocessing import prepare_validation_features
+from mrc.postprocessing import post_processing_function
 
 logger = logging.getLogger(__name__)
 
@@ -45,8 +43,6 @@ def main():
         (ModelArguments, DataTrainingArguments, TrainingArguments)
     )
     model_args, data_args, training_args = parser.parse_args_into_dataclasses()
-
-    training_args.do_train = True
 
     print(f"model is from {model_args.model_name_or_path}")
     print(f"data is from {data_args.dataset_name}")
@@ -80,6 +76,7 @@ def main():
         else model_args.model_name_or_path,
         use_fast=True,
     )
+
     model = AutoModelForQuestionAnswering.from_pretrained(
         model_args.model_name_or_path,
         from_tf=bool(".ckpt" in model_args.model_name_or_path),
@@ -91,64 +88,10 @@ def main():
         datasets = run_sparse_retrieval(
             tokenizer.tokenize, datasets, training_args, data_args,
         )
-
-    # eval or predict mrc model
-    if training_args.do_eval or training_args.do_predict:
-        run_mrc(data_args, training_args, model_args, datasets, tokenizer, model)
-
-
-def run_sparse_retrieval(
-    tokenize_fn: Callable[[str], List[str]],
-    datasets: DatasetDict,
-    training_args: TrainingArguments,
-    data_args: DataTrainingArguments,
-    data_path: str = "../data",
-    context_path: str = "wikipedia_documents.json",
-) -> DatasetDict:
-
-    # Query에 맞는 Passage들을 Retrieval 합니다.
-    retriever = SparseRetrieval(
-        tokenize_fn=tokenize_fn, data_path=data_path, context_path=context_path
-    )
-    retriever.get_sparse_embedding()
-
-    if data_args.use_faiss:
-        retriever.build_faiss(num_clusters=data_args.num_clusters)
-        df = retriever.retrieve_faiss(
-            datasets["validation"], topk=data_args.top_k_retrieval
-        )
-    else:
-        df = retriever.retrieve(datasets["validation"], topk=data_args.top_k_retrieval)
-
-    # test data 에 대해선 정답이 없으므로 id question context 로만 데이터셋이 구성됩니다.
+        
+    # predict mrc model
     if training_args.do_predict:
-        f = Features(
-            {
-                "context": Value(dtype="string", id=None),
-                "id": Value(dtype="string", id=None),
-                "question": Value(dtype="string", id=None),
-            }
-        )
-
-    # train data 에 대해선 정답이 존재하므로 id question context answer 로 데이터셋이 구성됩니다.
-    elif training_args.do_eval:
-        f = Features(
-            {
-                "answers": Sequence(
-                    feature={
-                        "text": Value(dtype="string", id=None),
-                        "answer_start": Value(dtype="int32", id=None),
-                    },
-                    length=-1,
-                    id=None,
-                ),
-                "context": Value(dtype="string", id=None),
-                "id": Value(dtype="string", id=None),
-                "question": Value(dtype="string", id=None),
-            }
-        )
-    datasets = DatasetDict({"validation": Dataset.from_pandas(df, features=f)})
-    return datasets
+        run_mrc(data_args, training_args, model_args, datasets, tokenizer, model)
 
 
 def run_mrc(
@@ -176,50 +119,21 @@ def run_mrc(
         data_args, training_args, datasets, tokenizer
     )
 
-    # Validation preprocessing / 전처리를 진행합니다.
-    def prepare_validation_features(examples):
-        # truncation과 padding(length가 짧을때만)을 통해 toknization을 진행하며, stride를 이용하여 overflow를 유지합니다.
-        # 각 example들은 이전의 context와 조금씩 겹치게됩니다.
-        tokenized_examples = tokenizer(
-            examples[question_column_name if pad_on_right else context_column_name],
-            examples[context_column_name if pad_on_right else question_column_name],
-            truncation="only_second" if pad_on_right else "only_first",
-            max_length=max_seq_length,
-            stride=data_args.doc_stride,
-            return_overflowing_tokens=True,
-            return_offsets_mapping=True,
-            # return_token_type_ids=False, # roberta모델을 사용할 경우 False, bert를 사용할 경우 True로 표기해야합니다.
-            padding="max_length" if data_args.pad_to_max_length else False,
-        )
-
-        # 길이가 긴 context가 등장할 경우 truncate를 진행해야하므로, 해당 데이터셋을 찾을 수 있도록 mapping 가능한 값이 필요합니다.
-        sample_mapping = tokenized_examples.pop("overflow_to_sample_mapping")
-
-        # evaluation을 위해, prediction을 context의 substring으로 변환해야합니다.
-        # corresponding example_id를 유지하고 offset mappings을 저장해야합니다.
-        tokenized_examples["example_id"] = []
-
-        for i in range(len(tokenized_examples["input_ids"])):
-            # sequence id를 설정합니다 (to know what is the context and what is the question).
-            sequence_ids = tokenized_examples.sequence_ids(i)
-            context_index = 1 if pad_on_right else 0
-
-            # 하나의 example이 여러개의 span을 가질 수 있습니다.
-            sample_index = sample_mapping[i]
-            tokenized_examples["example_id"].append(examples["id"][sample_index])
-
-            # context의 일부가 아닌 offset_mapping을 None으로 설정하여 토큰 위치가 컨텍스트의 일부인지 여부를 쉽게 판별할 수 있습니다.
-            tokenized_examples["offset_mapping"][i] = [
-                (o if sequence_ids[k] == context_index else None)
-                for k, o in enumerate(tokenized_examples["offset_mapping"][i])
-            ]
-        return tokenized_examples
+    prepare_validation_features_partial = partial(prepare_validation_features, 
+                                                  tokenizer=tokenizer,
+                                                  data_args=data_args,
+                                                  question_column_name=question_column_name,
+                                                  context_column_name=context_column_name,
+                                                  pad_on_right=pad_on_right,
+                                                  max_seq_length=max_seq_length,
+                                                  answer_column_name=answer_column_name
+                                                  )
 
     eval_dataset = datasets["validation"]
 
     # Validation Feature 생성
     eval_dataset = eval_dataset.map(
-        prepare_validation_features,
+        prepare_validation_features_partial,
         batched=True,
         num_proc=data_args.preprocessing_num_workers,
         remove_columns=column_names,
@@ -233,38 +147,12 @@ def run_mrc(
         tokenizer, pad_to_multiple_of=8 if training_args.fp16 else None
     )
 
-    # Post-processing:
-    def post_processing_function(
-        examples,
-        features,
-        predictions: Tuple[np.ndarray, np.ndarray],
-        training_args: TrainingArguments,
-    ) -> EvalPrediction:
-        # Post-processing: start logits과 end logits을 original context의 정답과 match시킵니다.
-        predictions = postprocess_qa_predictions(
-            examples=examples,
-            features=features,
-            predictions=predictions,
-            max_answer_length=data_args.max_answer_length,
-            output_dir=training_args.output_dir,
-        )
-        # Metric을 구할 수 있도록 Format을 맞춰줍니다.
-        formatted_predictions = [
-            {"id": k, "prediction_text": v} for k, v in predictions.items()
-        ]
-
-        if training_args.do_predict:
-            return formatted_predictions
-        elif training_args.do_eval:
-            references = [
-                {"id": ex["id"], "answers": ex[answer_column_name]}
-                for ex in datasets["validation"]
-            ]
-
-            return EvalPrediction(
-                predictions=formatted_predictions, label_ids=references
-            )
-
+    post_processing_function_partial = partial(post_processing_function,
+                                               data_args=data_args, 
+                                               answer_column_name=answer_column_name, 
+                                               datasets=datasets
+                                               )
+    
     metric = load_metric("squad")
 
     def compute_metrics(p: EvalPrediction) -> Dict:
@@ -280,29 +168,19 @@ def run_mrc(
         eval_examples=datasets["validation"],
         tokenizer=tokenizer,
         data_collator=data_collator,
-        post_process_function=post_processing_function,
+        post_process_function=post_processing_function_partial,
         compute_metrics=compute_metrics,
     )
 
-    logger.info("*** Evaluate ***")
+    logger.info("*** Inference ***")
 
     #### eval dataset & eval example - predictions.json 생성됨
     if training_args.do_predict:
         predictions = trainer.predict(
             test_dataset=eval_dataset, test_examples=datasets["validation"]
         )
-
         # predictions.json 은 postprocess_qa_predictions() 호출시 이미 저장됩니다.
-        print(
-            "No metric can be presented because there is no correct answer given. Job done!"
-        )
-
-    if training_args.do_eval:
-        metrics = trainer.evaluate()
-        metrics["eval_samples"] = len(eval_dataset)
-
-        trainer.log_metrics("test", metrics)
-        trainer.save_metrics("test", metrics)
+        print("Retrieval + MRC prediction done!")
 
 
 if __name__ == "__main__":
--- mrc/postprocessing.py
+++ mrc/postprocessing.py
@@ -1,10 +1,22 @@
+from typing import Tuple
+
+import numpy as np
+
 from transformers import EvalPrediction
+from transformers import TrainingArguments
 
 from utils_qa import postprocess_qa_predictions
 
 # Post-processing:
-def post_processing_function(examples, features, predictions, training_args, 
-                             data_args, answer_column_name, datasets):
+def post_processing_function(
+        examples, 
+        features, 
+        predictions: Tuple[np.ndarray, np.ndarray],
+        training_args: TrainingArguments,
+        data_args, 
+        answer_column_name, 
+        datasets
+) -> EvalPrediction:
     # Post-processing: start logits과 end logits을 original context의 정답과 match시킵니다.
     predictions = postprocess_qa_predictions(
         examples=examples,
